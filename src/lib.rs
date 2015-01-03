514c37a4a7142d1627f34bde01b5d266dbd14cdd
Jonathan Reem <jonathan.reem@gmail.com>
2015-01-03 23:33:26 +0100

    (fix) Add a Sized bound to Typeable.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@
 use std::intrinsics::TypeId;
 
 /// Universal mixin trait for adding a `get_type` method.
-pub trait Typeable: 'static {
+///
+// FIXME: Remove the `Sized` bound here, it should be unnecessary.
+// Tracking: rust-lang/rust#20492
+pub trait Typeable: Sized + 'static {
     /// Get the `TypeId` of this object.
     fn get_type(&self) -> TypeId { TypeId::of::<Self>() }
 }
